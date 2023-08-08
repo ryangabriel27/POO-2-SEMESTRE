53dec6c6fb046f1ccf77621bda138aa97c87963a
ryangabriel27 <ryanjulio56@gmail.com>
2023-08-08 16:56:54 -0300

    08/08

--- Exception/ExemploThrow.java
+++ Exception/ExemploThrow.java
@@ -3,23 +3,36 @@ package Exception;
 import javax.swing.JOptionPane;
 
 public class ExemploThrow {
-    public static void main(String[] args) throws Exception { 
-        try {
-           JOptionPane.showMessageDialog(null, "Bem-vindo ao SENAI!");
+    public static void main(String[] args) throws Exception {
+
+        JOptionPane.showMessageDialog(null, "Bem-vindo ao SENAI!");
         String login = JOptionPane.showInputDialog("Informe seu login:");
         String dataNasc = JOptionPane.showInputDialog("Digite sua data de nascimento [dd/mm/aa]");
-        String senha = JOptionPane.showInputDialog("Digite sua senha:");
-        dataNasc.replace("/","");
-        if (senha.length()!= 6){
-            throw new Exception("A senha deve conter 6 dígitos!!!");
-        }  else if (senha.equals(login)) {
-            throw new Exception("A senha não pode ser igual ao nome!");
-        }  else if (senha.equals(dataNasc)) {
-            throw new Exception("A senha não pode ser igual a data de nascimento");
-        } 
-        } catch (Exception e) {
-            // TODO: handle exception
-            JOptionPane.showMessageDialog(null, e.getMessage());
+        boolean aberto = true;
+        while (aberto) {
+            try {
+                String senha = JOptionPane.showInputDialog("Digite sua senha:");
+                dataNasc = dataNasc.replace("/", "");
+
+                if (senha.length() != 6) {
+                    throw new Exception("A senha deve conter 6 dígitos!!!");
+                }
+                if (senha.equals(login)) {
+                    throw new Exception("A senha não pode ser igual ao nome!");
+                }
+                if (senha.equals(dataNasc)) {
+                    throw new Exception("A senha não pode ser igual a data de nascimento!");
+                }
+
+                if (!senha.matches(".*[a-z].*") || !senha.matches(".*[A-Z].*") ) {
+                    throw new Exception("A senha deve conter pelo menos uma letra minúscula e uma maiúscula");
+                }
+
+                aberto = false;
+
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, e.getMessage());
+            }
         }
     }
 }
