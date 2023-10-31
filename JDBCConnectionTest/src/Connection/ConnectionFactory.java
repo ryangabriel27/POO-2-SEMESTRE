00497880a98a22a7fddb552af68907559f9ab8ce
ryangabriel27 <ryangabriel27>
2023-10-31 17:03:01 -0300

    31.10

---
+++ JDBCConnectionTest/src/Connection/ConnectionFactory.java
@@ -0,0 +1,47 @@
+package Connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.management.RuntimeErrorException;
+
+public class ConnectionFactory {
+    // Atributos
+    private static final String url = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String usuario = "postgres";
+    private static final String senha = "postgres";
+
+    // métodos
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(url, usuario, senha);
+        } catch (Exception e) {
+            throw new RuntimeErrorException(null, "Erro ao obter conexão com o banco de dados");
+        }
+    }
+
+    public static void closeConnection(Connection connection) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void closeConnection(Connection connection, PreparedStatement stmt) {
+        try {
+            if (connection != null && stmt != null) {
+                stmt.close();
+                connection.close();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    
+}
